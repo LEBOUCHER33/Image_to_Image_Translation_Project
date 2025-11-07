969e186836a3528d8b61e2178c6f9c18a48ef215
Sophie LEBOUCHER <Sophie.LEBOUCHER@cea.fr>
2025-11-07 15:04:12 +0100

    update du script de l'analyse spatiale

--- Pix2Pix/Graphs/metriques_perf.py
+++ Pix2Pix/Graphs/metriques_perf.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 import numpy as np
-from Pix2Pix.Datasets_Processing.dataset_CFD import load_dataset, get_tf_dataset, resize_and_normalize
+from CFD_dataset_cas_2 import load_dataset, get_tf_dataset, resize_and_normalize
 import json
 import pandas as pd
 import tensorflow as tf
@@ -37,7 +37,8 @@ DEBUG = False
 # 1- Datasets
 
 dataset_name = "film_3_128x128.npy"
-dataset_path = Path("./git/ImageMLProject/Datasets/CFD_Dataset/") / dataset_name
+dataset_path = os.path.abspath("./git/ImageMLProject/Datasets/CFD_Dataset/" + dataset_name)
+dataset_path = Path(dataset_path)
 dataset_path = dataset_path.resolve()
 
 inputs, targets, train_idx, test_idx, val_idx = load_dataset(dataset_path, offset=1)
@@ -247,6 +248,18 @@ for i in range(x_test.shape[0]):
 
 def prediction_dataset(inputs, targets, v_min, v_max):
     """
+    _Summary_: fonction qui génère des predictions sur un dataset et calcule les erreurs globales
+    _Args_: 
+        - inputs (np.array): images d'entrée (shape (N,H,W,1))
+        - targets (np.array): images cibles (shape (N,H,W,1))
+        - v_min, v_max (float): bornes de la vitesse physique
+    _Returns_: 
+        - preds (np.array) : images predite [0,255]
+        - MAE de la vitesse (float)
+        - RMSE de la vitesse (float)
+        - liste des MAE individuelles (list)
+        - liste des RMSE individuelles (list)
+        - variance des MAE individuelles (float)
     
     """
     mae_list = []
@@ -359,9 +372,9 @@ plt.show()
 
 
 
-# ////////////////////////////////////////////
+# //////////////////////////////////////////////////////////////////////////////////////////
 # courbe des pertes globales sur les trois datasets avec affichage de la variance globale
-# ////////////////////////////////////////////
+# /////////////////////////////////////////////////////////////////////////////////////////
 
 
 all_data = []
@@ -410,6 +423,47 @@ plt.grid(True, linestyle='--', alpha=0.5)
 plt.tight_layout()
 #plt.savefig(os.path.join(output_dir, "courbe_des_pertes_mae_avec_variance.png"), dpi=300)
 plt.show()    
+
+
+# //////////////////////////
+# box_plot des MAE
+# //////////////////////////
+
+import seaborn as sns
+
+
+colors=sns.color_palette('coolwarm',3)
+data=[train_mae_list, val_mae_list, test_mae_list]
+labels=['train', 'val', 'test']
+plt.figure(figsize=(10, 8))
+box = plt.boxplot(
+    data,                   # argument positionnel obligatoire
+    vert=False,             # boxplots horizontaux
+    patch_artist=True,      # pour pouvoir colorer les boîtes
+    labels=labels,
+    showmeans=True,
+    meanline=True,
+    meanprops={'color': 'black', 'linewidth': 2},
+    medianprops={'color': 'black', 'linewidth': 1.5},
+    flierprops={'marker': 'o', 'markerfacecolor': 'blue', 'markersize': 5}
+)
+# Appliquer les couleurs
+for patch, color in zip(box['boxes'], colors):
+    patch.set_facecolor(color)
+
+mean_line = plt.Line2D([], [], color='black', linewidth=2, label='Mean')
+median_line = plt.Line2D([], [], color='black', linestyle='--', linewidth=2, label='Median')
+# Mise en forme
+plt.xlabel("MAE moyenne par image prédite [m/s]")
+plt.ylabel("Dataset")
+plt.title("Dispersion des erreurs absolues moyennes par prédiction sur les trois datasets", fontsize=13)
+plt.legend(loc='upper right')
+plt.grid(True, linestyle='--', alpha=0.5)
+plt.legend(handles=[mean_line, median_line], loc='upper right')
+plt.tight_layout()
+plt.savefig(os.path.join(output_dir, "boxplot_des_pertes_mae.png"), dpi=300, bbox_inches='tight')
+plt.show()
+
 
 
 # //////////////////////////////////
@@ -462,14 +516,13 @@ plt.show()
 
 
 
-
-
-# ////////////////////////////////////////////////
+# ////////////////////////////////////////////////////////////////////////                                                      
 # affichage des images / des erreurs absolues / des erreurs quadratiques
-# ///////////////////////////////////////////////
+# ////////////////////////////////////////////////////////////////////////
+from scipy import stats
 
 
-for i in range (3, x_test.shape[0], 30):
+for i in range (3, x_test.shape[0], 15):
     # Données d'entrée et cible
     input_i = x_test[i]
     tar_i = y_test[i]
@@ -479,21 +532,31 @@ for i in range (3, x_test.shape[0], 30):
     img_tar = np.squeeze((tar_i.numpy() + 1) * 127.5).astype(np.uint8)
     img_pred = np.squeeze(pred_i).astype(np.uint8)
     # Conversion en vitesses physiques
+    input_vitesse = np.squeeze((input_i.numpy() + 1) / 2) * (v_max - v_min) + v_min # [-1,1] -> [0,1] -> [v_min,v_max]
     pred_vitesse = np.squeeze(pred_i) / 255 * (v_max - v_min) + v_min # [0,255] -> [0,1] -> [v_min,v_max]
     tar_vitesse = np.squeeze((tar_i.numpy() + 1) / 2) * (v_max - v_min) + v_min # [-1,1] -> [0,1] -> [v_min,v_max]
    # Carte d’erreur mae : erreur locale absolue
     error_map_mae_rel = np.abs(pred_vitesse - tar_vitesse) / (np.maximum(0, np.abs(tar_vitesse)))
     error_map_mae_abs = np.abs(pred_vitesse - tar_vitesse)
+    error_rel_max = np.max(error_map_mae_rel)
     error_max = np.max(error_map_mae_abs)
     error_min = np.min(error_map_mae_abs)
     error_variance = np.var(error_map_mae_abs)
-    # Carte d’erreur rmse : erreur locale quadratique
+    # Carte d’erreur mse : erreur locale quadratique
     error_map_quad = np.square(pred_vitesse - tar_vitesse)
-    error_map_quad_root = np.sqrt(error_map_quad)
-    # statistiques globales sur l'image
     mae_mean = np.mean(error_map_mae_abs)
     mse_mean = np.mean(error_map_quad)
     rmse_mean = np.sqrt(mse_mean)
+    error_map_rmse = np.sqrt(error_map_quad)
+    # Carte d'affichage des gradients temporels des vitesses
+    grad_mag = np.abs(tar_vitesse - input_vitesse)
+    # compression 1D
+    grad_mag_1d = grad_mag.flatten()
+    error_map_mae_abs_1d = error_map_mae_abs.flatten()
+    mask = np.isfinite(error_map_mae_abs_1d) & np.isfinite(grad_mag_1d)
+    x=grad_mag_1d[mask]
+    y=error_map_mae_abs_1d[mask]
+    slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
     # affichage
     """
     On veut une échelle comparative entre les images et avec l'error_map
@@ -502,7 +565,7 @@ for i in range (3, x_test.shape[0], 30):
         - pourm'erreur mae relative je prends l'échelle 0/10 (on affiche les erreurs relatives inférieures à 10x la valeur réelle)
         - pour l'error_map_quad je prends l'échelle 0/max(error_map_quad.max)
     """
-    fig, ax = plt.subplots (1,3,sharey=True,figsize=(12,8))
+    fig, ax = plt.subplots (1,4,sharey=True,figsize=(15,8))
     # Échelle commune pour les vitesses
     vmin_img, vmax_img = 0, v_max
     # Images de vitesse
@@ -515,37 +578,44 @@ for i in range (3, x_test.shape[0], 30):
     im2 = ax[2].imshow(error_map_mae_abs, vmin=0, vmax=error_max)
     ax[2].set_title("MAE prediction")
     ax[2].set_axis_off()
+    im3 = ax[3].imshow(error_map_rmse, vmin=0, vmax=10)
+    ax[3].set_title("RMSE prediction")
+    ax[3].set_axis_off()
     fig.colorbar(im0, ax=ax[0], fraction=0.046, pad=0.04, label="m/s")
     fig.colorbar(im1, ax=ax[1], fraction=0.046, pad=0.04, label="m/s")
     fig.colorbar(im2, ax=ax[2], fraction=0.046, pad=0.04, label="m/s")
+    fig.colorbar(im3, ax=ax[3], fraction=0.046, pad=0.04, label="m/s")
+    # ---- ANNOTATIONS ----
     stats_text = (
         f"erreur_abs_max = {error_max:.3f} m/s\n"
         f"erreur_abs_min = {error_min:.3f} m/s\n"
-        f"erreur_abs_variance = {error_variance:.3f}\n"
+        f"erreur_abs_variance = {error_variance:.3f} (m/s)²\n"
         f"MAE moyenne = {mae_mean:.3f} m/s\n"
+        f"MSE moyenne = {mse_mean:.3f} (m/s)²\n"
+        f"RMSE moyenne = {rmse_mean:.3f} m/s"
     )
     fig.suptitle(f"Prediction {i} — Erreurs absolues sur la prédiction de vitesse", fontsize=16, y=0.95, color='black')
     fig.text(0.01, 0.85, stats_text, fontsize=12, color='black', va='top')
     fig.tight_layout()
     plt.savefig(os.path.join(output_dir, f'fig_pred_mae_abs_{i}.png'))
     # Cartes d'erreur
-    fig, ax = plt.subplots(1, 4, sharex=True, sharey=True, figsize=(15,8))    
+    fig, ax = plt.subplots(1, 4, sharex=True, sharey=True, figsize=(18,8))    
     im0 = ax[0].imshow(img_tar, vmin=0, vmax=vmax_img)
     ax[0].set_title("Target")
     ax[0].set_axis_off()
-    im1 = ax[1].imshow(error_map_mae_abs, vmin=0, vmax=error_max)
-    ax[1].set_title("MAE sur la vitesse")
+    im1 = ax[1].imshow(img_pred, vmin=0, vmax=vmax_img)
+    ax[1].set_title("Prediction")
     ax[1].set_axis_off()
-    im4 = ax[2].imshow(error_map_mae_rel, cmap="coolwarm", vmin=0, vmax=2)
+    im4 = ax[2].imshow(error_map_mae_rel, cmap="coolwarm", vmin=0, vmax=error_rel_max)
     ax[2].set_title("MAE relative")
     ax[2].set_axis_off()
-    im5 = ax[3].imshow(error_map_quad_root, cmap="coolwarm", vmin=0, vmax=error_map_quad.max())
-    ax[3].set_title("RMSE sur la vitesse (m²/s²)")
+    im5 = ax[3].imshow(grad_mag, cmap="coolwarm", vmin=0)
+    ax[3].set_title("Gradient de la vitesse")
     ax[3].set_axis_off()
     fig.colorbar(im0, ax=ax[0], fraction=0.046, pad=0.04, label="m/s")
     fig.colorbar(im1, ax=ax[1], fraction=0.046, pad=0.04, label="m/s")
     fig.colorbar(im4, ax=ax[2], fraction=0.046, pad=0.04, label= "mae relative" )
-    fig.colorbar(im5, ax=ax[3], fraction=0.046, pad=0.04, label="m/s")
+    fig.colorbar(im5, ax=ax[3], fraction=0.046, pad=0.04, label="m²/s²")
         # ---- ANNOTATIONS ----
     # Ajoute les statistiques dans la figure
     stats_text = (
@@ -556,14 +626,204 @@ for i in range (3, x_test.shape[0], 30):
     fig.suptitle(f"Prediction {i} — Erreurs sur la prédiction de vitesse", fontsize=16, y=0.95, color='black')
     fig.text(0.01, 0.85, stats_text, fontsize=12, color='black', va='top')
     fig.tight_layout()
-    plt.savefig(os.path.join(output_dir, f'fig_pred_mae-rmse_{i}.png'))
+    plt.savefig(os.path.join(output_dir, f'fig_pred_mae-gradient_{i}.png'))
+    # subplot quantification de l'erreur
+    fig, ax = plt.subplots(1,4, figsize=(15,4), gridspec_kw={'width_ratios': [1, 1, 1, 1]})
+    im0=ax[0].imshow(error_map_mae_rel, cmap="coolwarm", vmin=0, vmax=error_rel_max)
+    ax[0].set_title("MAE relative")
+    ax[0].set_axis_off()
+    fig.colorbar(im0, ax=ax[0],fraction=0.046, pad=0.04, label="mae relative")
+    im1=ax[1].imshow(grad_mag, cmap="coolwarm", vmin=0)
+    ax[1].set_title("Gradient de la vitesse")
+    ax[1].set_axis_off()
+    fig.colorbar(im1, ax=ax[1], fraction=0.046, pad=0.04, label="m/s/pixel")
+    # histogramme
+    counts, bins, patches = ax[3].hist(error_map_mae_abs.flatten(), bins=np.arange(0, error_max.max() + 1, 1),
+                               color='darkblue', alpha=0.7, edgecolor='black')
+    count_0 = counts[0]
+    count_0_ratio = (count_0 / counts.sum()) * 100
+    ax[3].annotate(f"{int(count_0)} pixels\n {count_0_ratio:.2f}%",
+                 xy=(bins[0], count_0),
+                 xycoords='data',
+                 xytext=(25,0.2),
+                 textcoords='offset points',
+                 color='darkblue',
+                 fontsize=8,
+                 bbox=dict(facecolor='white', alpha=0.8)
+    )
+    seuil = 9.2
+    pixels_sous_seuil = np.sum(error_map_mae_abs < seuil)
+    pixels_total = error_map_mae_abs.size
+    pourcentage_pixels_sous_seuil = (pixels_sous_seuil / pixels_total) * 100
+    fig.text(0.5,0.5,f"Pixels avec MAE < 5% d'erreur absolue: \n {pixels_sous_seuil} / {pourcentage_pixels_sous_seuil:.2f}%",
+                 fontsize=8, color='darkblue',
+                 transform=plt.gca().transAxes,
+                 ha='center', va='center',bbox=dict(facecolor='white', alpha=0.8))
+    ax[3].set_xlabel("Erreur absolue (m/s)")
+    ax[3].set_ylabel("Fréquence en nombre de pixels")
+    ax[3].set_xticks(np.arange(0, int(error_max.max()) + 1, 1))
+    ax[3].set_xlim(0, 10)
+    ax[3].set_ylim(0, counts.max()*1.1)
+    ax[3].set_title("Distribution des erreurs absolues")
+    x = grad_mag_1d[np.isfinite(error_map_mae_rel.flatten())]
+    y = error_map_mae_rel.flatten()[np.isfinite(error_map_mae_rel.flatten())]
+    slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+    ax[2].scatter(x, y, 
+                  color='darkblue', alpha=0.5, s=5)
+    ax[2].plot(x, slope * x + intercept, color='red', linestyle='--',
+               )
+    ax[2].set_xlabel("Gradient de la vitesse (m/s/pixel)")
+    ax[2].set_ylabel("Erreur relative (%)")
+    ax[2].set_title("Erreurs relatives vs gradients")
+    coeff_corr = np.corrcoef(grad_mag.flatten(), error_map_mae_abs.flatten())[0,1]
+    ax[2].text(0.05, 0.95, f"r = {coeff_corr:.3f}", transform=ax[2].transAxes,
+           fontsize=12, color='red', ha='left', va='top')
+    fig.tight_layout()
+    plt.savefig(os.path.join(output_dir, f'hist_pred_mae_hist_{i}.png'))
     plt.show()
     plt.close()
 
 
 
 
+# /////////////////////////////////////////////////////////////////////
+# calcul de la correlation erreurs absolues vs gradients sur le film
+# /////////////////////////////////////////////////////////////////////
 
+grad_list, error_list, error_rel_list = [], [], []
+
+for i in range(x_test.shape[0]):
+    input_i = x_test[i]
+    tar_i = y_test[i]
+    pred_i = preds_test[i]
+    input_vitesse = np.squeeze((input_i.numpy() + 1) / 2) * (v_max - v_min)
+    tar_vitesse = np.squeeze((tar_i.numpy() + 1) / 2) * (v_max - v_min)
+    pred_vitesse = np.squeeze(pred_i) / 255 * (v_max - v_min)
+    grad_mag = np.abs(tar_vitesse - input_vitesse)
+    error_map_mae_abs = np.abs(pred_vitesse - tar_vitesse)
+    error_map_rel = np.abs(pred_vitesse - tar_vitesse) / (np.abs(tar_vitesse))
+    error_map_rel*=100
+    error_rel_list.append(error_map_rel.flatten())
+    grad_list.append(grad_mag.flatten())
+    error_list.append(error_map_mae_abs.flatten())
+
+
+grad_all = np.concatenate(grad_list)
+error_all = np.concatenate(error_list)
+err_rel_all = np.concatenate(error_rel_list)
+# --- Suppression des NaN / Inf ---
+mask = np.isfinite(grad_all) & np.isfinite(error_all) & np.isfinite(err_rel_all)
+grad_all = grad_all[mask]
+error_all = error_all[mask]
+err_rel_all = err_rel_all[mask]
+
+
+# --- Correlation linéaire pour MAE ---
+coeff_corr = np.corrcoef(grad_all, error_all)[0,1]
+slope, intercept, r_value, p_value, std_err = stats.linregress(grad_all, error_all)
+
+
+# --- Correlation linéaire pour relative errors ---
+coeff_corr_rel = np.corrcoef(grad_all, err_rel_all)[0,1]
+s, i, r, p, std = stats.linregress(grad_all, err_rel_all)
+
+
+if DEBUG:
+    print(grad_all.shape, error_all.shape, coeff_corr)
+
+# //////////////////////////
+# Scatter_plot
+# //////////////////////////
+
+fig, ax = plt.subplots(1,2,figsize=(10,6))
+ax[0].scatter(grad_all, error_all, alpha=0.5, s=2, color='royalblue', label='Predictions')
+ax[0].plot(grad_all, slope * grad_all + intercept, color='red', linestyle='--')
+ax[0].text(0.05,0.95,f"r={coeff_corr:.3f}",transform=ax[0].transAxes, fontsize=12)
+ax[0].set_ylabel("Erreur absolue (m/s)")
+ax[0].set_xlabel("Gradient de la vitesse (m/s/pixel)")
+ax[0].set_title("Erreurs absolues vs gradients")
+ax[0].legend()
+ax[1].scatter(grad_all, err_rel_all, alpha=0.5, s=2, color='royalblue', label='Predictions')
+ax[1].plot(grad_all, s * grad_all + i, color='red', linestyle='--')
+ax[1].text(0.05,0.95,f"r={coeff_corr_rel:.3f}",transform=ax[1].transAxes, fontsize=12)
+ax[1].set_ylabel("Erreur relative (%)")
+ax[1].set_xlabel("Gradient de la vitesse (m/s/pixel)")
+ax[1].set_title("Erreurs relatives vs gradients")
+fig.tight_layout()
+fig.suptitle("Correlation entre les erreurs et les gradients de vitesses du dataset de test")
+plt.savefig(os.path.join(output_dir, "scatter_plot_mae_gradient_rel.png"), dpi=300)
+plt.show()
+
+
+plt.figure(figsize=(6,6))
+plt.scatter(grad_all, error_all, alpha=0.5, s=2, color='royalblue', label='Predictions')
+plt.ylabel("Erreur absolue (m/s)")
+plt.xlabel("Gradient de la vitesse (m/s/pixel)")
+plt.plot(grad_all, slope * grad_all + intercept, color='red', linestyle='--')
+plt.text(0.05,0.95,f"r={coeff_corr:.3f}",transform=plt.gca().transAxes, fontsize=12)
+plt.title("Erreurs absolues vs gradients sur le dataset de test")
+plt.tight_layout()
+plt.savefig(os.path.join(output_dir, "scatter_plot_mae_gradient.png"), dpi=300)
+plt.show()
+
+
+# --- Correlation linéaire pour relative errors ---
+coeff_corr_rel = np.corrcoef(grad_all, err_rel_all)[0,1]
+slope, intercept, r_value, p_value, std_err = stats.linregress(grad_all, err_rel_all)
+
+plt.figure(figsize=(6,6))
+plt.scatter(grad_all, err_rel_all, alpha=0.5, s=2, color='royalblue', label='Predictions')
+plt.plot(grad_all, slope * grad_all + intercept, color='red', linestyle='--')
+plt.text(0.05,0.95,f"r={coeff_corr_rel:.3f}",transform=plt.gca().transAxes, fontsize=12)
+plt.ylabel("Erreur relative (%)")
+plt.xlabel("Gradient de la vitesse (m/s/pixel)")
+plt.title("Erreurs relatives vs gradients")
+plt.tight_layout()
+plt.savefig(os.path.join(output_dir, "scatter_plot_mae_rel_gradient.png"), dpi=300)
+plt.show()
+
+
+
+
+
+
+
+
+# //////////////////////////////////////
+# évaluation du biais du modèle
+# //////////////////////////////////////
+
+# réalisation des prédictions sur le dataset de test
+
+def predictions_test(dataset_test, v_min, v_max):
+    pred_list, tar_list = [], []
+    for i in range(dataset_test.shape[0]):
+        inp = dataset_test[i:i+1]
+        tar = dataset_test[i:i+1]
+        pred,_ ,_ ,_ , _= prediction(inp, tar, v_min, v_max)
+        pred_list.append(np.array(pred).flatten())
+        tar_list.append(np.array(tar).flatten())
+        pred_all = np.concatenate(pred_list)
+        tar_all = np.concatenate(tar_list)
+    return pred_all, tar_all 
+
+pred_all, tar_all = predictions_test(x_test, v_min, v_max)
+
+
+# tracé du scatter plot
+
+plt.figure(figsize=(6,6))
+plt.scatter(tar_all, pred_all, alpha=0.5, s=2, color='royalblue', label='Predictions')
+plt.xlabel("vitesses réelles")
+plt.ylabel("vitesses prédites")
+# diagonale parfaite
+plt.plot([np.array(tar_all).min(), np.array(tar_all).max()],
+         [np.array(pred_all).min(), np.array(pred_all).max()],
+         lw=2, color='red')
+plt.title(f"diagramme des corrélations entre les vitesses prédites et réelles")
+plt.legend()
+plt.savefig(os.path.join(output_dir, "diagramme_des_correlations.png"), dpi=300)
+plt.show()
 
 
 
